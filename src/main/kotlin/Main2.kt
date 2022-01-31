4afbdf1f8ae8af364310b4c3316d7df59c8fbb53
Benjimg2783 <bmartinez@iesrafaelalberti.es>
2022-01-31 21:12:11 +0100

    finalizado

--- src/main/kotlin/Main2.kt
+++ src/main/kotlin/Main2.kt
@@ -7,15 +7,18 @@ fun randomArray():Array<Int?>{
     }
     return array
 }
+
 fun main() {
     val array = randomArray()
     for (i in array.indices){
-        if (array[i]!!%2==0) println(array[i])
+        if (array[i]!!%2==0) print(array[i])
     }
+    println()
     repeat(20){ print('*') };println()
     array.sort()
     println(array.first())
     println(array.last())
+    println( array.sumOf { it!!})
     repeat(20){ print('*') };println()
     for (i in array.indices){
         if (array[i]!!%2!=0) array[i]= null
